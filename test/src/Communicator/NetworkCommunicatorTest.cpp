7d1beed554807ff4805804db7dac16c4d7667f06
RJ <esquiviasrj@gmail.com>
2019-12-20 20:42:50 -0800

    Add test templates
    Added NetworkCommunicatorTest
    Added FileSystemTest

---
+++ test/src/Communicator/NetworkCommunicatorTest.cpp
@@ -0,0 +1,10 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(FileSystemTest, TestSend)
+{
+}
+
+TEST(FileSystemTest, TestReceive)
+{
+}
---
+++ test/src/FileSystemReceiver/FileSystemTest/FileSystemTest.cpp
@@ -0,0 +1,18 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(FileSystemTest, TestIsDirectory)
+{
+}
+
+TEST(FileSystemTest, TestIsAbsolutePath)
+{
+}
+
+TEST(FileSystemTest, TestExecute)
+{
+}
+
+TEST(FileSystemTest, TestReadIntoBuffer)
+{
+}
